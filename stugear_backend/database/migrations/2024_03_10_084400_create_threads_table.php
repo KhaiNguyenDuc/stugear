b9388575827d3e6c9b3615fc911b4a8fe9b1431b
DESKTOP-EB88TB7\TUANKIET <tuankietlenguyen2709@gmail.com>
2024-03-10 16:12:45 +0700

    feature/be_thread:  complete create extra table for thread, reply and react

---
+++ stugear_backend/database/migrations/2024_03_10_084400_create_threads_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->id();
+            $table->text('description')->nullable();
+            $table->longText('content')->nullable();
+            $table->unsignedBigInteger('image_id')->nullable();
+            $table->foreign('image_id')->references('id')->on('images');
+            $table->integer('view')->nullable();
+            $table->integer('like')->nullable();
+            $table->integer('dislike')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->dateTime('created_at')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->dateTime('updated_at')->nullable();
+            $table->integer('deleted_by')->nullable();
+            $table->dateTime('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('threads');
+    }
+};
