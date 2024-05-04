3bc0d31cbce4424a4dddf23eb61da5217d99703b
KhaiNguyenDuc <duckhailinux@gmail.com>
2024-05-04 12:24:21 +0700

    Update path

--- stugear_frontend/src/utils/Constant.js
+++ stugear_frontend/src/utils/Constant.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "https://stugear.website/api";
-// export default BASE_API_URL = "https://stugear.website/api";
+export const BASE_API_URL = "https://stugear-qa.website/api";
+// export default BASE_API_URL = "https://stugear-qa.website/api";
