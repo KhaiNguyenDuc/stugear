cfae9ef0b0c150552b9548343610966e9c319f0b
DESKTOP-EB88TB7\TUANKIET <tuankietlenguyen2709@gmail.com>
2024-07-09 20:54:40 +0700

    finish upload and get multiple image products

--- stugear_backend/app/Util/ImageService.php
+++ stugear_backend/app/Util/ImageService.php
@@ -11,47 +11,50 @@ class ImageService
 {
     public static function uploadImage(Request $request, $id, $path, $tableNameOfImage)
     {
-        if ($request->has('image')){
+        if ($request->has('image')) {
             $image = $request->image;
-            $imageName = time() . '.' . $request->image->extension();
-            $image->move(public_path($path), $imageName);
+
+            $imgRef = $image[0];
+            $imageName = $tableNameOfImage . '_' . $id . '_' . time() . '.' . $imgRef->extension();
+            sleep(1);
+            $imgRef->move(public_path($path), $imageName);
             self::saveImage($imageName, $id, $path, $tableNameOfImage);
 
             try {
-                $imageId = DB::table('images')->where('title', $imageName)->value('id');
-                if (!$imageId){
-                    $a = DB::table('images')->insert([
-                        'path' => $path . '/' . $imageName,
-                        'title' => $imageName,
-                        'created_at' => Carbon::now(),
-                        'updated_at' => Carbon::now()
-                    ]);
-                    $imageId = DB::table('images')->where('title', $imageName)->value('id');
+                for($i = 1; $i < count($image); $i++) {
+                    $img = $image[$i];
+                    $imageName = $tableNameOfImage . '_' . $id . '_' . time() . '.' . $img->extension();
+                    sleep(1);
+                    $img->move(public_path($path), $imageName);
+                    try {
+                        $imageId = DB::table('images')->where('title', $imageName)->value('id');
+                        if (!$imageId) {
+                            $a = DB::table('images')->insert([
+                                'path' => $path . '/' . $imageName,
+                                'title' => $imageName,
+                                'created_at' => Carbon::now(),
+                                'updated_at' => Carbon::now()
+                            ]);
 
-                    $b = DB::table($tableNameOfImage)
-                        ->where('id', $id)
-                        ->update([
-                            'image_id' => $imageId
-                        ]);
-                    // use a, b, c, d for debug if nessessary
-                } else {
-                    $c = DB::table('images')
-                        ->update([
-                            'updated_at' => Carbon::now()
-                        ]);
+                        } else {
+                            $c = DB::table('images')
+                                ->update([
+                                    'updated_at' => Carbon::now()
+                                ]);
 
-                    $d = DB::table($tableNameOfImage)
-                        ->where('id', $id)
-                        ->update([
-                            'updated_at' => Carbon::now()
-                        ]);
+                            $d = DB::table($tableNameOfImage)
+                                ->where('id', $id)
+                                ->update([
+                                    'updated_at' => Carbon::now()
+                                ]);
+                        }
+                    } catch (\Throwable $th) {
+                        return AppConstant::$UPLOAD_FAILURE;
+                    }
                 }
             } catch (\Throwable $th) {
                 return AppConstant::$UPLOAD_FAILURE;
             }
-
-
-
 
             return AppConstant::$UPLOAD_SUCCESS;
         }
